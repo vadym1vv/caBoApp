5ae26ebb27a91a9c1dec7f511d3435dcc0ee5b69
vadym1vv <vadym1vv@gmail.com>
2025-12-10 16:00:02 +0200

    clean project

---
+++ CaBoApp/CaBoApp/CaBoAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  CaBoAppApp.swift
+//  CaBoApp
+//
+//  Created by vadym vasylaki on 10.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CaBoAppApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+              
+        }
+    }
+}
---
+++ CaBoApp/CaBoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CaBoApp
+//
+//  Created by vadym vasylaki on 10.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
